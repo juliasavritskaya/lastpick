4dda74c37f86c1788572bcc6a1f4222f3a0749b5
juliasavritskaya <juliamainpotterhead@gmail.com>
2022-02-22 01:30:16 +0300

    Initial commit

---
+++ src/main/java/by/mnkqn/lastpick/LastpickApplication.java
@@ -0,0 +1,13 @@
+package by.mnkqn.lastpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/mnkqn/lastpick/LastpickApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mnkqn.lastpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
