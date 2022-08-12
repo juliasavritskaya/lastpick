22d5e4e649daf5547b996c39c09845d21f3a9427
juliasavritskaya <mnkqn0211>
2022-08-12 21:32:11 +0400

    Connect Hero with database entity

--- src/main/java/by/mnkqn/lastpick/model/entity/Hero.java
+++ src/main/java/by/mnkqn/lastpick/model/entity/Hero.java
@@ -2,13 +2,31 @@ package by.mnkqn.lastpick.model.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "heroes")
 public class Hero {
+
+    @Id
     private Long id;
+
+    @Column(name = "name", nullable = false, length = 60)
     private String name;
-    private String publicName;
-    private AttackType attackType;
+
+    @Column(name = "localized_name", nullable = false, length = 60)
+    private String localizedName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "primary_attribute", nullable = false, length = 60)
     private Attribute primaryAttribute;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "attack_type", nullable = false, length = 60)
+    private AttackType attackType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "position", nullable = false, length = 60)
     private Position position;
-    private Float winrate;
 }
