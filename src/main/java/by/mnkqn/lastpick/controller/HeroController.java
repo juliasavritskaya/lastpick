b59cfa23011a2f5a49e048dfc9737bc6768f7404
juliasavritskaya <mnkqn0211>
2022-08-14 03:52:43 +0400

    Add sample method for retrieving data from opendota api

--- src/main/java/by/mnkqn/lastpick/controller/HeroController.java
+++ src/main/java/by/mnkqn/lastpick/controller/HeroController.java
@@ -1,9 +1,27 @@
 package by.mnkqn.lastpick.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/test")
 public class HeroController {
 
+    String baseUrl = "https://api.opendota.com/api";
+
+    @GetMapping
+    public ResponseEntity<String> findheroes() {
+        RestTemplate template = new RestTemplate();
+        String fooResourceUrl
+                = baseUrl + "/heroes";
+        ResponseEntity<String> response
+                = template.getForEntity(fooResourceUrl, String.class);
+        return response;
+    }
 
 }
