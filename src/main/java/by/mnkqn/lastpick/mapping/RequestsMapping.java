f829dcc1f33088364315f07fc65cb998952fe63a
juliasavritskaya <mnkqn0211>
2022-08-14 18:13:44 +0400

    Add SAVE_HEROES_TO_DB mapping

---
+++ src/main/java/by/mnkqn/lastpick/mapping/RequestsMapping.java
@@ -0,0 +1,8 @@
+package by.mnkqn.lastpick.mapping;
+
+public class RequestsMapping {
+
+  public static final String BASE_URL = "api/v1";
+  public static final String HEROES_URL = BASE_URL + "/heroes";
+  public static final String SAVE_HEROES_TO_DB = HEROES_URL + "/transferherolist";
+}
